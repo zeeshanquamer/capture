5b45730855d6f8539cd326e15d83d1ee63d291fc
zeeshanquamer <zeesh654@gmail.com>
2023-08-24 21:31:52 +0530

    completed

---
+++ src/components/FaqSection.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "styled-components";
+import { About } from "../styles";
+import Toggle from "./Toggle";
+import { LayoutGroup } from "framer-motion";
+
+const FaqSection = () => {
+  return (
+    <Faq>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <LayoutGroup>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+              autem accusamus ex laboriosam porro, adipisci quam voluptatum
+              magnam placeat corporis.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error,
+              totam.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+      </LayoutGroup>
+    </Faq>
+  );
+};
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
+
+export default FaqSection;
